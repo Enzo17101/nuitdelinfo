a819eaa7309f78d54edc69fec9f55ad37752a473
MatteoLanglois <mamac49@free.fr>
2021-12-03 00:56:26 +0100

    update footer

--- DefiPrincipal/index.php
+++ DefiPrincipal/index.php
@@ -110,44 +110,6 @@
 
     <div class="parallax"></div>
 
-    <footer>
-      <div class="leftFooter">
-        <div class="leftUpFooter">
-          <div class="contact">
-            <p id="titleFooter">Contact:</p>
-            <p id="email">sauveteurdudunkerquois@gmail.com</p>
-            <a href="https://www.facebook.com/groups/938396409644949" class="fa fa-facebook"></a>
-            <a href="https://twitter.com/boutelierphili1" class="fa fa-twitter"></a>
-          </div>
-          <div class="partenaires">
-            <p id="titleFooter">Partenaires:</p>
-            <a href="https://www.snsm.org/"><img src="data/logo_SNSM.svg" alt=""></a>
-            <a href="http://www.les-corsaires-dunkerquois.com/"><img src="data/corsaires_dunkerquois_logo.png" alt=""></a>
-          </div>
-          <form action="" method="get" class="newsletter_form">
-            <p class="newsletter_title">S'abonner à la newsletter du site</p>
-            <div class="newsletter">
-              <div class="coordonnees">
-                <label for="nom">Nom :</label>
-                <input type="text" name="nom" id="nom" required>
-                <label for="mail">E-mail :</label>
-                <input type="text" name="mail" id="mail" required>
-              </div>
-              <input type="submit" value="S'abonner !">
-            </div>
-          </form>
-
-          </div>
-        </div>
-        <div>
-          <span>Livre d'or - Plan du Site</span>
-        </div>
-      </div>
-      <div class="news">
-      </div>
-    </footer>
-
-
 
   </body>
 </html>
