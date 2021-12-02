b9d62198708c95450a684f0a77655c1503bc0042
Matteo Langlois <langlois.matteo49@gmail.com>
2021-12-02 19:43:22 +0100

    add a picture for the top of the website

--- DefiPrincipal/index.php
+++ DefiPrincipal/index.php
@@ -9,71 +9,75 @@
   </head>
   <body>
     <header>
-      <div class="dropdown">
-       <button class="dropbtn">Sauveteurs</button>
-       <div class="dropdown-content">
-         <a href="#">Tableau d'honneur</a>
-         <a href="#">Qui étaient-ils ?</a>
-         <a href="#">Que faisaient-ils ?</a>
-         <a href="#">Paroles de</a>
-         <a href="#">Gratifications</a>
-         <a href="#">Sauveteurs ailleurs</a>
-       </div>
-      </div>
-      <div class="dropdown">
-       <button class="dropbtn">Sorties en mer</button>
-       <div class="dropdown-content">
-         <a href="#">18ème siècle</a>
-         <a href="#">19ème siècle</a>
-         <a href="#">20ème siècle</a>
-         <a href="#">21ème siècle</a>
-       </div>
-      </div>
-      <div class="dropdown">
-       <button class="dropbtn">Stations</button>
-       <div class="dropdown-content">
-         <a href="#">Dunkerque</a>
-         <a href="#">Gravelines</a>
-         <a href="#">Fort-Mardyck</a>
-         <a href="#">Malo-les-bains</a>
-         <a href="#">Bray-Dunes</a>
-         <a href="#">Sauveteurs-ailleurs</a>
-       </div>
-      </div>
-      <div class="dropdown">
-       <button class="dropbtn">Services du port</button>
-       <div class="dropdown-content">
-         <a href="#">Dunkerque</a>
-       </div>
-      </div>
-      <div class="dropdown">
-       <button class="dropbtn">Moyens maritimes</button>
-       <div class="dropdown-content">
-         <a href="#">Dunkerque</a>
-       </div>
-      </div>
-      <div class="dropdown">
-       <button class="dropbtn">Techniques</button>
-       <div class="dropdown-content">
-         <a href="#">Dunkerque</a>
-       </div>
-      </div>
-      <div class="dropdown">
-       <button class="dropbtn">Historique</button>
-       <div class="dropdown-content">
-         <a href="#">Dunkerque</a>
-       </div>
-      </div>
-      <div class="dropdown">
-       <button class="dropbtn">Compléments</button>
-       <div class="dropdown-content">
-         <a href="#">Dunkerque</a>
-       </div>
-      </div>
-      <div class="dropdown">
-       <button class="dropbtn">Estaminet</button>
-      </div>
+      <img src="data/Banniere.jpg" alt="Plage de dunkerque">
+      <nav>
+        <div class="dropdown">
+         <button class="dropbtn">Sauveteurs</button>
+         <div class="dropdown-content">
+           <a href="#">Tableau d'honneur</a>
+           <a href="#">Qui étaient-ils ?</a>
+           <a href="#">Que faisaient-ils ?</a>
+           <a href="#">Paroles de</a>
+           <a href="#">Gratifications</a>
+           <a href="#">Sauveteurs ailleurs</a>
+         </div>
+        </div>
+        <div class="dropdown">
+         <button class="dropbtn">Sorties en mer</button>
+         <div class="dropdown-content">
+           <a href="#">18ème siècle</a>
+           <a href="#">19ème siècle</a>
+           <a href="#">20ème siècle</a>
+           <a href="#">21ème siècle</a>
+         </div>
+        </div>
+        <div class="dropdown">
+         <button class="dropbtn">Stations</button>
+         <div class="dropdown-content">
+           <a href="#">Dunkerque</a>
+           <a href="#">Gravelines</a>
+           <a href="#">Fort-Mardyck</a>
+           <a href="#">Malo-les-bains</a>
+           <a href="#">Bray-Dunes</a>
+           <a href="#">Sauveteurs-ailleurs</a>
+         </div>
+        </div>
+        <div class="dropdown">
+         <button class="dropbtn">Services du port</button>
+         <div class="dropdown-content">
+           <a href="#">Dunkerque</a>
+         </div>
+        </div>
+        <div class="dropdown">
+         <button class="dropbtn">Moyens maritimes</button>
+         <div class="dropdown-content">
+           <a href="#">Dunkerque</a>
+         </div>
+        </div>
+        <div class="dropdown">
+         <button class="dropbtn">Techniques</button>
+         <div class="dropdown-content">
+           <a href="#">Dunkerque</a>
+         </div>
+        </div>
+        <div class="dropdown">
+         <button class="dropbtn">Historique</button>
+         <div class="dropdown-content">
+           <a href="#">Dunkerque</a>
+         </div>
+        </div>
+        <div class="dropdown">
+         <button class="dropbtn">Compléments</button>
+         <div class="dropdown-content">
+           <a href="#">Dunkerque</a>
+         </div>
+        </div>
+        <div class="dropdown">
+         <button class="dropbtn">Estaminet</button>
+        </div>
+      </nav>
     </header>
+
 
   </body>
 </html>
