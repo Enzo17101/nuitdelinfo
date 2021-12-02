5f7a253f7e41b04bb2a7c268a013071c20122d5c
MatteoLanglois <mamac49@free.fr>
2021-12-03 00:10:31 +0100

    move footer into an other file

--- DefiPrincipal/index.php
+++ DefiPrincipal/index.php
@@ -137,29 +137,7 @@
 
     <div class="parallax"></div>
 
-    <footer>
-      <div class="leftFooter">
-        <div class="leftUpFooter">
-          <div class="contact">
-            <p id="titleFooter">Contact:</p>
-            <p id="email">sauveteurdudunkerquois@gmail.com</p>
-            <a href="https://www.facebook.com/groups/938396409644949" class="fa fa-facebook"></a>
-            <a href="https://twitter.com/boutelierphili1" class="fa fa-twitter"></a>
-          </div>
-          <div class="partenaires">
-            <p id="titleFooter">Partenaires:</p>
-            <a href="https://www.snsm.org/"><img src="data/logo_SNSM.svg" alt=""></a>
-            <a href="http://www.les-corsaires-dunkerquois.com/"><img src="data/corsaires_dunkerquois_logo.png" alt=""></a>
-          </div>
-          <div class=""></div>
-        </div>
-        <div>
-          <span>Livre d'or - Plan du Site</span>
-        </div>
-      </div>
-      <div class="news">
-      </div>
-    </footer>
+    <?php include '/nuitdelinfo/DefiPrincipal/php/footer.php' ?>
 
 
 
