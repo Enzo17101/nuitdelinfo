7961203d6ed5516048117bbd9fd645910c21b8fe
Matteo Langlois <langlois.matteo49@gmail.com>
2021-12-02 19:25:51 +0100

    begin the main project

---
+++ DefiPrincipal/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Sauveteurs du dunkerquois</title>
+  </head>
+  <body>
+    <header>
+      <div class="dropdown">
+       <button class="dropbtn">Sauveteurs</button>
+       <div class="dropdown-content">
+         <a href="#">Tableau d'honneur</a>
+         <a href="#">Qui étaient-ils</a>
+         <a href="#">Que faisaient-ils</a>
+         <a href="#">Paroles de</a>
+         <a href="#">Gratifications</a>
+         <a href="#">Sauveteurs ailleurs</a>
+       </div>
+      </div>
+      <div class="dropdown">
+       <button class="dropbtn">Sorties en mer</button>
+       <div class="dropdown-content">
+         <a href="#">18ème siècle</a>
+         <a href="#">19ème siècle</a>
+         <a href="#">20ème siècle</a>
+         <a href="#">21ème siècle</a>
+       </div>
+      </div>
+      <div class="dropdown">
+       <button class="dropbtn">Stations</button>
+       <div class="dropdown-content">
+         <a href="#">Dunkerque</a>
+         <a href="#">Gravelines</a>
+         <a href="#">Fort-Mardyck</a>
+         <a href="#">Malo-les-bains</a>
+         <a href="#">Bray-Dunes</a>
+         <a href="#">Sauveteurs-ailleurs</a>
+       </div>
+      </div>
+    </header>
+
+  </body>
+</html>
