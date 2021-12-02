8f4e2a196ae0c3b7663d05035d586fd936cfbdbf
MatteoLanglois <mamac49@free.fr>
2021-12-02 20:50:33 +0100

    update span

--- DefiPrincipal/index.php
+++ DefiPrincipal/index.php
@@ -86,14 +86,15 @@
 
 
     <span class="main">
-      <p>Bienvenue sur le site des sauveteurs du dunkerquois. Ce site rend
-        hommage aux femmes, hommes et enfants qui ont réalisé des actes de
-        sauvetages en milieu aquatique.</p>
-      <p>Ces sauveteurs, habitants du dunkerquois (de Bray-Dunes à
-        Grand-Fort-Philippe), ont participé à plus de 900 sauvetages en mer et
-        plus de 1100 sauvetages individuels. Œuvrant avec courage, abnégation et
-         souvent au mépris du risque ils méritent amplement que leurs actes
-         soient pérennisés. </p>
+      Bienvenue sur le site des sauveteurs du dunkerquois. Ce site rend
+      hommage aux femmes, hommes et enfants qui ont réalisé des actes de
+      sauvetages en milieu aquatique.
+      <br>
+      Ces sauveteurs, habitants du dunkerquois (de Bray-Dunes à
+      Grand-Fort-Philippe), ont participé à plus de 900 sauvetages en mer et
+      plus de 1100 sauvetages individuels. Œuvrant avec courage, abnégation et
+      souvent au mépris du risque ils méritent amplement que leurs actes
+      soient pérennisés.
     </span>
 
 
