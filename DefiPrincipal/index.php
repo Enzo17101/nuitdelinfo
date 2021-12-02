e20bfd87b9c29c397aae35860f36950b6803d4b2
MatteoLanglois <mamac49@free.fr>
2021-12-03 00:19:51 +0100

    update link for css

--- DefiPrincipal/index.php
+++ DefiPrincipal/index.php
@@ -4,10 +4,9 @@
     <meta charset="utf-8">
     <title>Sauveteurs du dunkerquois</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="/nuitdelinfo/DefiPrincipal/css/dropdown.css">
-    <link rel="stylesheet" href="/nuitdelinfo/DefiPrincipal/css/color1.css">
-    <link rel="stylesheet" href="/nuitdelinfo/DefiPrincipal/css/style.css">
-    <link rel="stylesheet" href="/nuitdelinfo/DefiPrincipal/css/footer.css">
+    <link rel="stylesheet" href="/var/www/html/nuitdelinfo/DefiPrincipal/css/dropdown.css">
+    <link rel="stylesheet" href="/var/www/html/nuitdelinfo/DefiPrincipal/css/color1.css">
+    <link rel="stylesheet" href="/var/www/html/nuitdelinfo/DefiPrincipal/css/style.css">
   </head>
   <body>
     <div class="parallax"></div>
