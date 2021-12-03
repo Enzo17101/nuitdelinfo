b900bafe97d2e2fda6c1bde21b13844172d2fdf9
MatteoLanglois <mamac49@free.fr>
2021-12-03 01:04:45 +0100

    update

--- DefiPrincipal/index.php
+++ DefiPrincipal/index.php
@@ -101,7 +101,7 @@
       </nav>
     </header>
 
-    <div class="parallax-2"></div>
+    <div class="parallax2"></div>
 
     <?php
     if ($_SERVER['SCRIPT_NAME'] == '/nuitdelinfo/DefiPrincipal/index.php') {
--- DefiPrincipal/php/main.php
+++ DefiPrincipal/php/main.php
@@ -12,7 +12,7 @@
   </span>
 </div>
 
-<div class="parallax-2"></div>
+<div class="parallax2"></div>
 
 <div class="captiontxt">
   <span class="main">
